1d03d48277396c613efba4de78d49727f0502cce
dzy <dzy@macrohard.fun>
2025-10-02 02:35:41 +0800

    fixes

--- panel/public/weapon_templates/TwinLightAxes.lua
+++ panel/public/weapon_templates/TwinLightAxes.lua
@@ -6,7 +6,7 @@ Weapon:new({
     switch_delay = Delay.LONG,
     number = Weapon.MELEE,
     purchase_sequence = {},
-    last_whirl_time = 0,
+    last_light_vortex_time = 0,
     template_name = "TwinLightAxes",
     ---@param self TwinLightAxes
     ---@param round integer
