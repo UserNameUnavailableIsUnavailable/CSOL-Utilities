4fb5a7e9ad070abca657361b8737284e9459e5f9
dzy <ttzbig@126.com>
2026-05-10 23:52:17 +0800

    protobuf support

---
+++ Controller/include/Server.hpp
@@ -0,0 +1,20 @@
+#include "Protos/Controller/Configuration.pb.h"
+
+namespace CSOL_Utilities
+{
+    class Server
+    {
+    public:
+        Server(const std::string& address, const std::string& port);
+        ~Server() noexcept;
+
+        void Start();
+        void Stop();
+        void RegisterService(const std::string& service_name);
+
+    private:
+        std::string address_;
+        std::string port_;
+        // Additional members for server implementation (e.g., socket, threads, etc.)
+    };
+}
--- make/cmake.mk
+++ make/cmake.mk
@@ -6,32 +6,35 @@ CMAKE_BINARY_DIR ?=
 CMAKE_INSTALL_PREFIX ?=
 CMAKE_CONFIG_TYPE ?=
 
-# 检查上述字段是否已定义
+CMAKE_PLATFORM := x64
+CMAKE_TOOLSET := host=x64
+# generator **MUST** support multi-config, otherwise the build type will be ignored and all targets will be built in Debug mode by default, which is not what we want.
+# Since this project runs on Windows only, Visual Studio 17 2022 or higher is recommended.
+CMAKE_GENERATOR := Visual Studio 18 2026
+
+# These settings are mandatory
 ifndef CMAKE_SOURCE_DIR
 $(error CMAKE_SOURCE_DIR is not defined!)
 endif
 ifndef CMAKE_BINARY_DIR
 $(error CMAKE_BINARY_DIR is not defined!)
 endif
-ifndef CMAKE_INSTALL_PREFIX
-$(error CMAKE_INSTALL_PREFIX is not defined!)
+ifndef VERSION
+$(error VERSION is not defined!)
 endif
 ifndef CMAKE_CONFIG_TYPE
 $(error CMAKE_CONFIG_TYPE is not defined!)
 endif
-ifndef VERSION
-$(error VERSION is not defined!)
-endif
 
-# CMake settings
-export CMAKE_SUPPORT_MULTI_CONFIG := true
-export CMAKE_PLATFORM := x64
-export CMAKE_TOOLSET := host=x64
-# generator **MUST** support multi-config, otherwise the build type will be ignored and all targets will be built in Debug mode by default, which is not what we want.
-# Since this project runs on Windows, Visual Studio 17 2022 or higher is recommended.
-export CMAKE_GENERATOR := Visual Studio 18 2026
-export CMAKE_CONFIGURE_ARGS = -S "$(CMAKE_SOURCE_DIR)" -B "$(CMAKE_BINARY_DIR)" -G "$(CMAKE_GENERATOR)" -A "$(CMAKE_PLATFORM)" -T "$(CMAKE_TOOLSET)" -DVERSION="$(VERSION)"
-export CMAKE_BUILD_ARGS = --build $(CMAKE_BINARY_DIR) --config "$(CMAKE_CONFIG_TYPE)"
-export CMAKE_INSTALL_ARGS = --install $(CMAKE_BINARY_DIR) --prefix "$(CMAKE_INSTALL_PREFIX)" --config "$(CMAKE_CONFIG_TYPE)"
+CMAKE_CONFIGURE_ARGS = -S "$(CMAKE_SOURCE_DIR)" -B "$(CMAKE_BINARY_DIR)" -G "$(CMAKE_GENERATOR)" -A "$(CMAKE_PLATFORM)" -T "$(CMAKE_TOOLSET)" -DVERSION="$(VERSION)"
+
+CMAKE_BUILD_ARGS = --build $(CMAKE_BINARY_DIR) --config "$(CMAKE_CONFIG_TYPE)"
+
+CMAKE_INSTALL_ARGS = --install $(CMAKE_BINARY_DIR) --config "$(CMAKE_CONFIG_TYPE)"
+
+# customize install dir
+ifdef CMAKE_INSTALL_PREFIX
+CMAKE_INSTALL_ARGS += --prefix "$(CMAKE_INSTALL_PREFIX)" 
+endif
 
 endif # !CMAKE_MK
